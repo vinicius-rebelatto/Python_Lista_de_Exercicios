e19ea7ad4e20cab514ef853f2199c9c5fbf9f708
Vinicius Rebelatto <50716786+vinicius-rebelatto@users.noreply.github.com>
2025-08-04 20:53:50 -0300

    added ex008

--- ex008.py
+++ ex008.py
@@ -1,8 +1,10 @@
 totAluno = int(input("Informe quantos alunos há na turma: "))
 maior = 0
 menor = 0
+tot = 0
 for i in range(0, totAluno):
     nota = float(input(f"Informe a nota do {i+1}° aluno: "))
+    tot += nota
     #define o maior se não for edfinido anteriormente
     if i == 0:
         maior, menor = nota
@@ -12,3 +14,6 @@ for i in range(0, totAluno):
         maior = nota
     if nota < menor:
         menor = nota
+print(f"maior nota: {maior}")
+print(f"menor nota: {menor}")
+print(f"media da turma: {tot / totAluno}")
